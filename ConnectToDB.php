7bae91c664fa64fe06e13f259ff93dd5ec2670fe
binhdoquoc <binhdq@fpt.edu.vn>
2019-04-25 19:02:04 +0700

    connect to local and Remote

--- ConnectToDB.php
+++ ConnectToDB.php
@@ -5,6 +5,7 @@
 <h1>My first PHP page</h1>
 
 <?php
+ini_set('display_errors', 1);
 echo "Hello abc XYZ!";
 ?>
 
@@ -18,20 +19,23 @@ echo "<h2>" . $txt1 . "</h2>";
 echo "Study PHP at " . $txt2 . "<br>";
 echo $x + $y;
 
- 
+
+
+if (empty(getenv("DATABASE_URL"))){
+    $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
+}  else {
+   $db = parse_url(getenv("DATABASE_URL"));
+   $pdo = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+   ));
+}  
+
 $sql = "SELECT id, name FROM label";
-
-$db = parse_url(getenv("DATABASE_URL"));
-
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
-
 $stmt = $pdo->prepare($sql);
 //Thiết lập kiểu dữ liệu trả về
 $stmt->setFetchMode(PDO::FETCH_ASSOC);
