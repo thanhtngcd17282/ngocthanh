99509d84bc7727a1db5e2c919302c7e6dea33a7a
Thanh <thanhtngcd17282@fpt.edu.vn>
2019-07-30 14:42:42 +0700

    thaydoi.index

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <title>PHP Test</title>
  </head>
  <body>
+     <h1> Xin chào các bạn, Mình là Thành đây </h1>
  <?php echo '<p>Hello World from Netbeans</p>'; 
     putenv("DATABASE_URL=fred");
     echo getenv("DATABASE_URL")
