07f7e56ab0d7b82d046a38c5beca4dd98ba5ab91
Thanh <thanhtngcd17282@fpt.edu.vn>
2019-07-30 15:00:41 +0700

    thaydoi.index

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
  </head>
  <body>
      <h1> Xin chào các bạn, Mình là Thành đây </h1>
- <?php echo '<p>Mọi thông tin chi tiết, hãy liên hệ tôi ở facebook nhé <3</p>'; 
-    putenv("DATABASE_URL=https://www.facebook.com/ngocthanh.tran.3954");
-    echo getenv("DATABASE_URL")
  ?> 
      <a href="https://www.facebook.com/ngocthanh.tran.3954">
          Chân Trần
+     </a>
+     <a href="https://ngocthanh0106.herokuapp.com/">
+         Trần Ngọc Thành
      </a>
      
  </body>
