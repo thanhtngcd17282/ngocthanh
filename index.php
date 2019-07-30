8342c8799f730df97dd91a87bf6b5526e8c7ace9
Thanh <thanhtngcd17282@fpt.edu.vn>
2019-07-30 15:17:58 +0700

    thaydoi.index

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <html>
  <head>
   <title>Trần Ngọc Thành/title>\
-      <style>
-          *{
-              margin: 0;
-              parding: 0;
-          }
-          body
-          {
-              background: ulr(ntn.jpg);
-                  
-          }
-      </style>
  </head>
  <body>
      <h1> Xin chào các bạn, Mình là Thành đây </h1>
