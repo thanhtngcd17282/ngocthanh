504584ff7c6efd86e860869f53d8c984960326c4
Thanh <thanhtngcd17282@fpt.edu.vn>
2019-07-30 15:13:17 +0700

    thaydoi.index

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <html>
  <head>
-  <title>PHP Test</title>
+  <title>Trần Ngọc Thành/title>\
+      <style>
+          *{
+              margin: 0;
+              parding: 0;
+          }
+          body
+          {
+              background: ulr(https://www.facebook.com/photo.php?fbid=980042325488881&set=a.100914416735014&type=3&theater);
+                  
+          }
+      </style>
  </head>
  <body>
      <h1> Xin chào các bạn, Mình là Thành đây </h1>
@@ -8,9 +19,6 @@
      <a href="https://www.facebook.com/ngocthanh.tran.3954">
          Chân Trần
      </a>
-     <a href="https://ngocthanh0106.herokuapp.com/">
-         Trần Ngọc Thành
-     </a>
-     
+         
  </body>
 </html>
